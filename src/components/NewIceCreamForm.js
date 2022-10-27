351ad3d55bce0e69ca82f16b7fa13fea9641127d
Bradbuchholz <bradbuchholz@gmail.com>
2022-10-26 20:40:49 -0700

    update NewIceCreamForm

---
+++ src/components/NewIceCreamForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import ReusableForm from "./ReusableForm";
+
+function NewIceCreamForm(props){
+
+  function handleNewIceCreamFormSubmission(event){
+    event.preventDefault();
+    props.onNewInventoryCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: parseFloat(event.target.price.value),
+      quantity: parseFloat(event.target.quantity.value),
+      id: v4()
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewIceCreamFormSubmission}
+        buttonText="Add To Inventory" />
+    </React.Fragment>
+  );
+}
+
+NewIceCreamForm.propTypes = {
+  onNewInventoryCreation: PropTypes.func
+};
+
+export default NewIceCreamForm;
